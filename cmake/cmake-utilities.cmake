5aaac978c05ea503a6e21fae75626c466150e2e1
Brandon Taylor <brandon.taylor221@gmail.com>
2024-03-26 19:15:59 -0400

    move tests

--- cmake/cmake-utilities.cmake
+++ cmake/cmake-utilities.cmake
@@ -149,3 +149,65 @@ function(try_add_config_compile_option OPTION_CONFIG OPTION)
         add_compile_options("$<$<AND:$<COMPILE_LANGUAGE:CXX>,$<CONFIG:${OPTION_CONFIG}>>:${OPTION}>")
     endif()
 endfunction()
+
+# Generate a python test from a python file
+# You can use generator expressions in the python file, like "$<TARGET_FILE:csbeats>"
+# You can import tools from test_helpers, for example:
+# `from test_helpers import check_input, run_command, check_output`
+function(generate_python_test test_name)
+    file(GENERATE OUTPUT "$<TARGET_FILE_DIR:csound-bin>/${test_name}.py"
+        INPUT "${test_name}.py.in"
+    )
+
+    add_test(
+        NAME ${test_name}
+        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+        COMMAND ${Python3_EXECUTABLE}
+            "$<TARGET_FILE_DIR:csound-bin>/${test_name}.py"
+    )
+
+    set_property(TEST ${test_name} PROPERTY LABELS ${ARGN})
+endfunction()
+
+# Add a csound test based on a .csd file
+# in the same folder as the CMakeLists.txt file you call the function from 
+# 
+# Arguments:
+# - filename: the filename of the .csd file
+# - status: one of 
+#     - "none": generates no output .wav file
+#     - "indeterminate": generates an output .wav file that changes from run to run
+#     - "consistent": generates a consistent output .wav file
+# - labels: any number of labels for the test. You can use labels to filter which tests to run. e.g ctest -L my_label
+# 
+# so if filename is "array_copy", then the csd file will be "array_copy.csd"
+# 
+# There are three steps to running the tests:
+# 1) Generate a .wav file from the csound file
+#   If the status is "none", trying to generate a .wav file should yield no output. Otherwise:
+# 
+# 2) Calculate the checksum of the .wav file
+#   Even a very small change in the .wav file will lead to a different checksum
+# 
+# 3a) If no checksum file exists, store the checksum
+#   save the checksum in a new file in the same folder as the .csd file
+#   If the filename is "array_copy", the checksum file will be "array_copy.wav.checksum"
+#
+# 3b) Else, compare the checksum to a stored checksum
+#   If the status is "consistent", compare the new checksum to a previous saved checksum
+#   If the checksums don't match, the test will fail
+#   So if someone makes a change such that generated .wav files no longer sound right,
+#   the tests will fail
+# 
+# If you want to regenerate all checksums, temporarily set REGENERATE_CHECKSUMS = True
+# in the tests/integration.py.in file
+function(add_integration_test test_name status)
+    add_test(
+        NAME ${test_name}
+        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+        COMMAND ${Python3_EXECUTABLE} "$<TARGET_FILE_DIR:csound-bin>/integration.py"
+        ${test_name} ${status}
+    )
+
+    set_property(TEST ${test_name} PROPERTY LABELS ${ARGN})
+endfunction()
